fe86370df4b246a1105cb04bd3e8a30790b10a00
christinexiaxiaxia <christine-xia@hotmail.com>
2026-06-15 08:50:14 -0400

    update

--- js/thingz2b.js
+++ js/thingz2b.js
@@ -1,3 +1,13 @@
+// BLURRING
+
 $('*').click(function(){
 	$('body').toggleClass('blur');
 })
+
+
+// TYPING
+
+$('body').on('keypress', function (e) {
+    console.log(e.keyCode);
+    $('.paper').append('<p>Test</p>')
+});
