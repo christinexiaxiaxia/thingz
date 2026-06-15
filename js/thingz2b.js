c9c122de488d8f247d6d1ef8c3cbd112408097ee
christinexiaxiaxia <christine-xia@hotmail.com>
2026-06-15 09:36:16 -0400

    update

--- js/thingz2b.js
+++ js/thingz2b.js
@@ -7,7 +7,31 @@ $('*').click(function(){
 
 // TYPING
 
-$('body').on('keypress', function (e) {
-    console.log(e.keyCode);
-    $('.paper').append('<p>Test</p>')
+var aLower = document.getElementById('a-lower'),
+	aLowerClone = aLower.cloneNode(true);
+
+aLowerClone.id = 'a-lower-clone';
+
+$('html').on('keypress', function (e) {
+    // console.log(e.keyCode);
+
+    // TYPING LETTERS
+
+    if (event.keyCode == 97) { // i have to make an array for the key codes and one for the ids, then create a loop so i don't have to brute force lol, i have to push myself to do this
+    	console.log(e.keyCode);
+    	$('#a-lower').clone().appendTo('.paper');
+    }
 });
+
+
+
+
+
+// BACKSPACING
+
+$('html').keyup(function(e){
+	if (e.keyCode == 8) {
+    	console.log("backspace");
+    	$('.paper').children().last().remove();
+	}
+})  
