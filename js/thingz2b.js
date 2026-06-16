b169f67c431300ba4776e784d73f7f1196c9f868
christinexiaxiaxia <christine-xia@hotmail.com>
2026-06-15 20:07:17 -0400

    update

--- js/thingz2b.js
+++ js/thingz2b.js
@@ -1,7 +1,73 @@
+const keyLower = [
+	'#a-lower',
+	'#b-lower',
+	'#c-lower',
+	'#d-lower',
+	'#e-lower',
+	'#f-lower',
+	'#g-lower',
+	'#h-lower',
+	'#i-lower',
+	'#j-lower',
+	'#k-lower',
+	'#l-lower',
+	'#m-lower',
+	'#n-lower',
+	'#o-lower',
+	'#p-lower',
+	'#q-lower',
+	'#r-lower',
+	'#s-lower',
+	'#t-lower',
+	'#u-lower',
+	'#v-lower',
+	'#w-lower',
+	'#x-lower',
+	'#y-lower',
+	'#z-lower'
+]
+
+const keyUpper = [
+	'#a-upper',
+	'#b-upper',
+	'#c-upper',
+	'#d-upper',
+	'#e-upper',
+	'#f-upper',
+	'#g-upper',
+	'#h-upper',
+	'#i-upper',
+	'#j-upper',
+	'#k-upper',
+	'#l-upper',
+	'#m-upper',
+	'#n-upper',
+	'#o-upper',
+	'#p-upper',
+	'#q-upper',
+	'#r-upper',
+	'#s-upper',
+	'#t-upper',
+	'#u-upper',
+	'#v-upper',
+	'#w-upper',
+	'#x-upper',
+	'#y-upper',
+	'#z-upper'
+]
+
+
+
+$(document).mousemove(function(e){
+	$('.cursor').css({'top': e.clientY + 10, 'left': e.clientX + 10})
+})
+
+
+
 // BLURRING
 
 $('*').click(function(){
-	$('body').toggleClass('blur');
+	$('.paper').toggleClass('blur');
 })
 
 
@@ -17,57 +83,46 @@ $('*').click(function(){
 // } );
 
 
+// VISIBILITY
 
-// TYPING
+$(document).ready(function(e){
+    for (i = 0; i < 26; i++) {
+    	$(keyLower[i]).addClass('bye-letter');
+    	$(keyUpper[i]).addClass('bye-letter');
+    	$('#space').addClass('bye-letter');
+    }
+})
 
-var aLower = document.getElementById('a-lower'),
-	aLowerClone = aLower.cloneNode(true);
-
-aLowerClone.id = 'a-lower-clone';
 
 
 // TYPING LETTERS
 
-$('html').on('keypress', function (e) {
-
-    const keyID = [
-    	'#a-lower',
-    	'#b-lower',
-    	'#c-lower',
-    	'#d-lower',
-    	'#e-lower',
-    	'#f-lower',
-    	'#g-lower',
-    	'#h-lower',
-    	'#i-lower',
-    	'#j-lower',
-    	'#k-lower',
-    	'#l-lower',
-    	'#m-lower',
-    	'#n-lower',
-    	'#o-lower',
-    	'#p-lower',
-    	'#q-lower',
-    	'#r-lower',
-    	'#s-lower',
-    	'#t-lower',
-    	'#u-lower',
-    	'#v-lower',
-    	'#w-lower',
-    	'#x-lower',
-    	'#y-lower',
-    	'#z-lower'
-    ]
+$(document).on('keypress', function (e) {
 
     for (i = 0; i < 26; i++) {
     	if (event.keyCode == i+97) {
     		console.log(e.keyCode);
-    		$(keyID[i]).clone().appendTo('.paper').addClass('hello-letter');;
+    		$(keyLower[i]).clone().appendTo('.paper').addClass('hello-letter').removeClass('bye-letter');
+    	}
+
+    	if (event.keyCode == i+65) {
+    		console.log(e.keyCode + 'shift');
+    		$(keyUpper[i]).clone().appendTo('.paper').addClass('hello-letter').removeClass('bye-letter');
     	}
     }
 });
 
 
+
+
+
+// SPACE
+
+$('html').keydown(function(e){
+	if (e.keyCode == 32) {
+    	$('#space').clone().appendTo('.paper').addClass('hello-letter').removeClass('bye-letter');
+	}
+})  
 
 
 
