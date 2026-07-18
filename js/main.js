f13ab38a422a13c120ad0a3aa41e4457a86ff386
christinexiaxiaxia <christine-xia@hotmail.com>
2026-07-18 11:33:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,42 @@
 // })
 
 
+// ON LOAD
+
+$(document).ready(function(){
+	$('.collapse').next().addClass('closed');
+	$('.collapse').next().addClass('indent');
+})
+
+
 // CLICK TO EXPAND + SHRINK COLUMNS
 
 $('.shelf').on('click', function(){
-	$(this).toggleClass('expand');
-	$('.item', this).toggleClass('expand-type');
+	if (!$('.collapse').hasClass('hover-button')) { // add this so when un/collapsing the column doesn't shrink/expand
+		$(this).toggleClass('expand');
+		$('.item', this).toggleClass('expand-type');
 
-	$('.shelf').not(this).removeClass('expand');
-	$('.item').not($(this).find('.item')).removeClass('expand-type');
+		$('.shelf').not(this).removeClass('expand');
+		$('.item').not($(this).find('.item')).removeClass('expand-type');
+	} else {
+
+	}
+})
+
+
+
+// COLLAPSING BUTTON
+
+$('.collapse').on('click', function(){
+	if (!$(this).next().hasClass('open')) {
+		$(this).next().removeClass('closed')
+		$(this).next().addClass('open')
+	} else {
+		$(this).next().removeClass('open')
+		$(this).next().addClass('closed')
+	}
+})
+
+$('.collapse').hover(function(){
+	$(this).toggleClass('hover-button')
 })
