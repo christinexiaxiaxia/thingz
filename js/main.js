6d5dc1256b7cc2f4931e898780229bc36bf1102c
christinexiaxiaxia <christine-xia@hotmail.com>
2026-07-18 13:26:32 -0400

    update

--- js/main.js
+++ js/main.js
@@ -21,9 +21,11 @@ $('.shelf').on('click', function(){
 	if (!$('.collapse').hasClass('hover-button')) { // add this so when un/collapsing the column doesn't shrink/expand
 		$(this).toggleClass('expand');
 		$('.item', this).toggleClass('expand-type');
+		$('ul.item', this).children().toggleClass('expand-type'); // make list item type size follow the rest of the expanding type
 
 		$('.shelf').not(this).removeClass('expand');
 		$('.item').not($(this).find('.item')).removeClass('expand-type');
+		$('ul.item').children().not($(this).find('li')).removeClass('expand-type'); // shrink type for all list items not in this shelf
 	} else {
 
 	}
